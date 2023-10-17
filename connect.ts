22b0c10c6f557b70461b272b1ed34bf8b3f2d6f0
Faisal Sayed <faisal.sayed502@gmail.com>
2023-10-17 14:29:01 -0400

    feat: get sprig data from slack channels

--- connect.ts
+++ connect.ts
@@ -1,24 +1,51 @@
 import { ConnectRouter } from "@connectrpc/connect";
 import { ElizaService } from "./gen/eliza_connect";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 import yaml from "js-yaml";
 import { Config } from "./lib/types";
+import { slack } from ".";
+import { ConversationsHistoryResponse } from "@slack/web-api";
 
 export default (router: ConnectRouter) =>
   router.service(ElizaService, {
     async syncSlackMessages(req, ctx) {
-      const input = req.project;
-      const config = yaml.load(readFileSync(`./config/${input}.yaml`, "utf-8")) as Config;
+      try {
+        // const input = req.project;
+        // const config = yaml.load(readFileSync(`./config/${input}.yaml`, "utf-8")) as Config;
+        // const channels = config["slack-channels"];
 
-      // 1. iterate over all slack channels
-      // 2. get all recent messages (100-200)
-      // 3. remove messages that are by THIS user
-      // 4. separate out the messages not responded yet in a separate array
-      // 5. calculate the average response time for each message (when the message was replied to, in threads)
-      // 6. make sure to exclude all the bot messages when calculating the average response time
+        // for (let i = 0; i < channels.length; i++) {
+        //   const channel = channels[i];
 
-      const channels = config.result.data["slack-channels"];
+        //   await slack.client.conversations.join({ channel: channel.id });
+        //   const messages = await slack.client.conversations.history({
+        //     channel: channel.id,
+        //     limit: 500,
+        //   });
+        // }
 
-      return { response: "ok" };
+        // testing with two files right now:
+        const sprigJson = JSON.parse(
+          readFileSync(`sprig.json`, "utf-8")
+        ) as ConversationsHistoryResponse;
+
+        const sprigPlatformJson = JSON.parse(
+          readFileSync(`sprig-platform.json`, "utf-8")
+        ) as ConversationsHistoryResponse;
+
+        for (let i = 0; i < sprigJson.messages!.length; i++) {
+          const message = sprigJson.messages![i];
+
+          if (message.bot_id || message.app_id) continue;
+          if (message.subtype) continue;
+
+          console.log(message.text);
+        }
+
+        return { response: "ok" };
+      } catch (err) {
+        console.error(err);
+        return { response: "An error occurred: " + err.message };
+      }
     },
   });
