e4d968fe293aacbcab051681039344b86f6024f6
Faisal Sayed <faisal.sayed502@gmail.com>
2023-11-03 11:20:00 -0400

    remove console log

--- index.ts
+++ index.ts
@@ -139,8 +139,6 @@ slack.event("message", async ({ event, client, logger, message }) => {
       });
     }
 
-    console.log(message);
-
     // B03QGF0H9FU = Airtable bot
     if (message.subtype || (message.bot_id && message.bot_id !== "B03QGF0H9FU")) return;
 
