fdc1bb84fce1ab93e75de15617ef4401717cbb73
Faisal Sayed <faisal.sayed502@gmail.com>
2024-01-10 10:33:47 -0500

    remove gimme button once its used

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,4 +1,4 @@
-import { Middleware, SlackAction, SlackActionMiddlewareArgs } from "@slack/bolt";
+import { Block, KnownBlock, Middleware, SlackAction, SlackActionMiddlewareArgs } from "@slack/bolt";
 import { StringIndexed } from "@slack/bolt/dist/types/helpers";
 import dayjs from "dayjs";
 import { Octokit } from "octokit";
@@ -406,7 +406,7 @@ export const gimmeAgain: Middleware<
   await ack();
 
   try {
-    const { user, channel, actions } = body as any;
+    const { user, channel, actions, message } = body as any;
     const command = actions[0].value as string;
 
     await handleSlackerCommand({
@@ -416,6 +416,28 @@ export const gimmeAgain: Middleware<
       client,
       logger,
       ...args,
+    });
+
+    if (!channel?.id) return;
+
+    const { messages } = await client.conversations.history({
+      channel: channel.id,
+      latest: message.ts,
+      limit: 1,
+      inclusive: true,
+    });
+
+    const blocks = messages?.[0].blocks || [];
+    const idx = blocks.findIndex(
+      (block: any) => block.elements && block.elements[0].action_id === "gimme_again"
+    );
+    const newBlocks = blocks.filter((_, i) => i !== idx && i !== idx + 1) as (Block | KnownBlock)[];
+
+    await client.chat.update({
+      ts: message.ts,
+      channel: channel.id,
+      text: `Message updated: ${message.id}`,
+      blocks: newBlocks,
     });
   } catch (err) {
     logger.error(err);
